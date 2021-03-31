73d2d1d8f48c3ba403ed1127494eafa0f66e8877
nickgray06 <nickgray87@gmail.com>
2021-03-30 20:22:26 -0600

    stripe connect not the right thing

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:stripe_connect]
+
+  def can_recieve_payments?
+    uid? && provider? && publishable_key?
+  end
+end
